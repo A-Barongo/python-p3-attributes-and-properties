df8664a8520ae184b8f9b924fb77305fd03fc925
Allan <allanbarongo5@gmail.com>
2025-05-15 14:06:53 +0300

    Completed lab on python attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name="lande",breed="Mastiff"):
+        if (type(name) is str) and (1<=len(name)<=25):
+            self.name=name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if breed in APPROVED_BREEDS:
+            self.breed=breed
+        else:
+            print("Breed must be in list of approved breeds.")
